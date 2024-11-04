a12b654f62ff9d01dc5f3ce76a785d6c8d188902
Ivan Kulik <kulik6515@gmail.com>
2024-11-04 14:30:46 +0300

    some fixes

--- rpd-client-ts/vite.config.ts
+++ rpd-client-ts/vite.config.ts
@@ -1,18 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: "http://localhost:8000"
-//       }
-//     }
-//   }
-// })
-
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
@@ -24,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || "http://rpd-server:8000",
+        target: process.env.VITE_API_URL || "http://server:8000",
         changeOrigin: true,
         secure: false,
       }
