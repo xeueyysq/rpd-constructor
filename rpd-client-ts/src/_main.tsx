567fd0c3a409b5e73589bf637ed36e89edc94b8c
Alexey Mikhaylov <black98alex@gmail.com>
2024-11-23 12:19:21 +0300

    Обновил конфиг резолва путей под FSD

--- rpd-client-ts/src/_main.tsx
+++ rpd-client-ts/src/_main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import AuthProvider from "./app/providers/AuthProvider.tsx";
-import CaslProvider from "./app/providers/CaslProvider.tsx";
+import AuthProvider from "@app/providers/AuthProvider";
+import CaslProvider from "@app/providers/CaslProvider";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
--- rpd-client-ts/vite.config.ts
+++ rpd-client-ts/vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react-swc'
 import {defineConfig} from 'vite'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,5 +19,15 @@ export default defineConfig({
     build: {
         outDir: "build",
         sourcemap: true,
-    }
+    },
+    resolve: {
+        alias: {
+            '@app': path.resolve(__dirname, 'src/app'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@widgets': path.resolve(__dirname, 'src/widgets'),
+            '@features': path.resolve(__dirname, 'src/features'),
+            '@entities': path.resolve(__dirname, 'src/entities'),
+            '@shared': path.resolve(__dirname, 'src/shared'),
+        },
+    },
 })
