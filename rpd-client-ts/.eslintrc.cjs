1e5d223b47353bfc1ba26ce9771d7a1cfcf3e65e
Alexey Mikhaylov <black98alex@gmail.com>
2024-12-02 21:38:06 +0300

    Добавил feature-sliced проверку в eslint

--- rpd-client-ts/.eslintrc.cjs
+++ rpd-client-ts/.eslintrc.cjs
@@ -1,18 +1,28 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        '@feature-sliced',
     ],
-  },
-}
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    settings: {
+        "import/resolver": {
+            typescript: {
+                alwaysTryTypes: true
+            }
+        }
+    },
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            {allowConstantExport: true},
+        ],
+        'import/order': ['off'],
+        'import/no-internal-modules': ['off'],
+    },
+};
