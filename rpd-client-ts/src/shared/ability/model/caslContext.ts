946fb8a93988e694989869921119128d4da70812
Alexey Mikhaylov <black98alex@gmail.com>
2024-12-09 20:59:52 +0300

    Fix CaslContext initial value

--- rpd-client-ts/src/shared/ability/model/caslContext.ts
+++ rpd-client-ts/src/shared/ability/model/caslContext.ts
@@ -1,4 +1,6 @@
 import {createContext} from 'react'
 import {AppAbility} from './types.ts'
+import {buildAbilityFor} from '../lib/buildAbilityFor.ts'
+import {UserRole} from './enums.ts'
 
-export const CaslContext = createContext<AppAbility | null>(null)
+export const CaslContext = createContext<AppAbility>(buildAbilityFor(UserRole.ANONYMOUS))
