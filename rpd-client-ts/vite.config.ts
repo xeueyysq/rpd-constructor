ec8f76fc22bc4085eda5fb605aab6c33b80d32c6
Ivan Kulik <kulik6515@gmail.com>
2025-01-18 15:54:17 +0300

    fix/vite.config.ts

--- rpd-client-ts/vite.config.ts
+++ rpd-client-ts/vite.config.ts
@@ -5,15 +5,18 @@ import path from 'path'
 export default defineConfig({
     plugins: [
         react({
-            jsxImportSource: '@emotion/react',
-            babel: {
-                plugins: ['@emotion/babel-plugin']
-            }
+            jsxImportSource: '@emotion/react'
         })
     ],
     resolve: {
         alias: {
-            '@emotion/styled': '@emotion/styled/base'
+            '@emotion/styled': '@emotion/styled/base',
+            '@app': path.resolve(__dirname, 'src/app'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@widgets': path.resolve(__dirname, 'src/widgets'),
+            '@features': path.resolve(__dirname, 'src/features'),
+            '@entities': path.resolve(__dirname, 'src/entities'),
+            '@shared': path.resolve(__dirname, 'src/shared')
         }
     },
     server: {
@@ -38,20 +41,7 @@ export default defineConfig({
                 }
             }
         },
-        outDir: 'dist',
-        sourcemap: false
+        outDir: 'build',
+        sourcemap: true
     }
-        outDir: "build",
-        sourcemap: true,
-    },
-    resolve: {
-        alias: {
-            '@app': path.resolve(__dirname, 'src/app'),
-            '@pages': path.resolve(__dirname, 'src/pages'),
-            '@widgets': path.resolve(__dirname, 'src/widgets'),
-            '@features': path.resolve(__dirname, 'src/features'),
-            '@entities': path.resolve(__dirname, 'src/entities'),
-            '@shared': path.resolve(__dirname, 'src/shared'),
-        },
-    },
 })
