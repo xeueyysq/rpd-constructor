83983cfe277e5be10a46d34bf2e1f967e9ee4c67
Ivan Kulik <kulik6515@gmail.com>
2025-08-15 19:11:00 +0300

    Вычисление года в создании комплекта

--- rpd-client-ts/src/features/select-template-data/ui/Selectors.tsx
+++ rpd-client-ts/src/features/select-template-data/ui/Selectors.tsx
@@ -8,268 +8,276 @@ import { enqueueSnackbar, VariantType } from "notistack";
 // import { TemplateConstructorType } from '@entities/template/index.ts'
 
 interface JsonData {
-  [key: string]: string | JsonData;
+    [key: string]: string | JsonData;
 }
 
 type Nullable<T> = T | undefined;
 
 interface SelectorsState {
-  faculty: Nullable<OptionType>;
-  levelEducation: Nullable<OptionType>;
-  directionOfStudy: Nullable<OptionType>;
-  profile: Nullable<OptionType>;
-  formEducation: Nullable<OptionType>;
-  year: Nullable<OptionType>;
+    faculty: Nullable<OptionType>;
+    levelEducation: Nullable<OptionType>;
+    directionOfStudy: Nullable<OptionType>;
+    profile: Nullable<OptionType>;
+    formEducation: Nullable<OptionType>;
+    year: Nullable<OptionType>;
 }
 
 interface Selectors {
-  setChoise: (value: string) => void;
+    setChoise: (value: string) => void;
 }
 
 export const Selectors: FC<Selectors> = ({ setChoise }) => {
-  const { setTabState } = useStore();
-  const { deInitializeTabs } = useStore();
-  const selectorsData = useStore.getState().selectedTemplateData;
-  const [selectors, setSelectors] = useState<SelectorsState>({
-    faculty: selectorsData.faculty
-      ? { value: selectorsData.faculty, label: selectorsData.faculty }
-      : undefined,
-    levelEducation: selectorsData.levelEducation
-      ? {
-          value: selectorsData.levelEducation,
-          label: selectorsData.levelEducation,
-        }
-      : undefined,
-    directionOfStudy: selectorsData.directionOfStudy
-      ? {
-          value: selectorsData.directionOfStudy,
-          label: selectorsData.directionOfStudy,
-        }
-      : undefined,
-    profile: selectorsData.profile
-      ? { value: selectorsData.profile, label: selectorsData.profile }
-      : undefined,
-    formEducation: selectorsData.formEducation
-      ? {
-          value: selectorsData.formEducation,
-          label: selectorsData.formEducation,
-        }
-      : undefined,
-    year: selectorsData.year
-      ? { value: selectorsData.year, label: selectorsData.year }
-      : undefined,
-  });
+    const { setTabState } = useStore();
+    const { deInitializeTabs } = useStore();
+    const selectorsData = useStore.getState().selectedTemplateData;
+    const [selectors, setSelectors] = useState<SelectorsState>({
+        faculty: selectorsData.faculty
+            ? { value: selectorsData.faculty, label: selectorsData.faculty }
+            : undefined,
+        levelEducation: selectorsData.levelEducation
+            ? {
+                  value: selectorsData.levelEducation,
+                  label: selectorsData.levelEducation,
+              }
+            : undefined,
+        directionOfStudy: selectorsData.directionOfStudy
+            ? {
+                  value: selectorsData.directionOfStudy,
+                  label: selectorsData.directionOfStudy,
+              }
+            : undefined,
+        profile: selectorsData.profile
+            ? { value: selectorsData.profile, label: selectorsData.profile }
+            : undefined,
+        formEducation: selectorsData.formEducation
+            ? {
+                  value: selectorsData.formEducation,
+                  label: selectorsData.formEducation,
+              }
+            : undefined,
+        year: selectorsData.year
+            ? { value: selectorsData.year, label: selectorsData.year }
+            : undefined,
+    });
 
-  const [data, setData] = useState<Nullable<JsonData>>(undefined);
-  const { setSelectedTemplateData } = useStore();
+    const [data, setData] = useState<Nullable<JsonData>>(undefined);
+    const { setSelectedTemplateData } = useStore();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/json_profiles.json", {
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-        });
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/json_profiles.json", {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                    },
+                });
 
-        const jsonData: JsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        const variant: VariantType = "error";
-        enqueueSnackbar("Ошибка загрузки профилей", { variant });
-      }
+                const jsonData: JsonData = await response.json();
+                setData(jsonData);
+            } catch (error) {
+                const variant: VariantType = "error";
+                enqueueSnackbar("Ошибка загрузки профилей", { variant });
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleChange =
+        (name: keyof SelectorsState) =>
+        (
+            _: React.SyntheticEvent<Element, Event>,
+            selectedOption: OptionType | null
+        ) => {
+            setSelectors((prevSelectors) => ({
+                ...prevSelectors,
+                [name]: selectedOption || undefined,
+                ...(name === "faculty" && {
+                    levelEducation: undefined,
+                    directionOfStudy: undefined,
+                    profile: undefined,
+                    formEducation: undefined,
+                    year: undefined,
+                }),
+                ...(name === "levelEducation" && {
+                    directionOfStudy: undefined,
+                    profile: undefined,
+                    formEducation: undefined,
+                    year: undefined,
+                }),
+                ...(name === "directionOfStudy" && {
+                    profile: undefined,
+                    formEducation: undefined,
+                    year: undefined,
+                }),
+                ...(name === "profile" && {
+                    formEducation: undefined,
+                    year: undefined,
+                }),
+                ...(name === "formEducation" && { year: undefined }),
+            }));
+        };
+
+    const getOptions = (): OptionType[] => {
+        return data
+            ? Object.keys(data).map((key) => ({ label: key, value: key }))
+            : [];
     };
 
-    fetchData();
-  }, []);
+    const getOptionsForSelector = (
+        selectorPath: Array<string>,
+        indicator?: string
+    ): OptionType[] => {
+        if (indicator && indicator === "lastChild") {
+            const currentYear = new Date().getFullYear();
+            const yearsCount = 5;
+            return Array.from({ length: yearsCount }, (_, i) => {
+                const year = currentYear + 1 - i;
+                return { label: String(year), value: String(year) };
+            });
+        }
+        if (!data) return [];
+        let currentData = data;
 
-  const handleChange =
-    (name: keyof SelectorsState) =>
-    (
-      _: React.SyntheticEvent<Element, Event>,
-      selectedOption: OptionType | null
-    ) => {
-      setSelectors((prevSelectors) => ({
-        ...prevSelectors,
-        [name]: selectedOption || undefined,
-        ...(name === "faculty" && {
-          levelEducation: undefined,
-          directionOfStudy: undefined,
-          profile: undefined,
-          formEducation: undefined,
-          year: undefined,
-        }),
-        ...(name === "levelEducation" && {
-          directionOfStudy: undefined,
-          profile: undefined,
-          formEducation: undefined,
-          year: undefined,
-        }),
-        ...(name === "directionOfStudy" && {
-          profile: undefined,
-          formEducation: undefined,
-          year: undefined,
-        }),
-        ...(name === "profile" && {
-          formEducation: undefined,
-          year: undefined,
-        }),
-        ...(name === "formEducation" && { year: undefined }),
-      }));
+        for (const key of selectorPath) {
+            currentData = currentData[key] as JsonData;
+            if (!currentData) {
+                return [];
+            }
+        }
+        return Object.keys(currentData).map((key) => ({
+            label: key,
+            value: key,
+        }));
     };
 
-  const getOptions = (): OptionType[] => {
-    return data
-      ? Object.keys(data).map((key) => ({ label: key, value: key }))
-      : [];
-  };
+    const saveTemplateData = () => {
+        setSelectedTemplateData(
+            selectors.faculty?.value,
+            selectors.levelEducation?.value,
+            selectors.directionOfStudy?.value,
+            selectors.profile?.value,
+            selectors.formEducation?.value,
+            selectors.year?.value
+        );
+        setChoise("workingType");
+        deInitializeTabs();
+        setTabState("workingType", true);
+    };
 
-  const getOptionsForSelector = (
-    selectorPath: Array<string>,
-    indicator?: string
-  ): OptionType[] => {
-    if (!data) return [];
-    let currentData = data;
+    // const selectedValue = useMemo(
+    //     () => all
+    // )
 
-    for (const key of selectorPath) {
-      currentData = currentData[key] as JsonData;
-      if (!currentData) {
-        return [];
-      }
-    }
-    if (indicator && indicator === "lastChild")
-      return Object.values(currentData).map((value) => ({
-        label: String(value),
-        value: String(value),
-      }));
-    return Object.keys(currentData).map((key) => ({ label: key, value: key }));
-  };
+    if (!data) return <Loader />;
 
-  const saveTemplateData = () => {
-    setSelectedTemplateData(
-      selectors.faculty?.value,
-      selectors.levelEducation?.value,
-      selectors.directionOfStudy?.value,
-      selectors.profile?.value,
-      selectors.formEducation?.value,
-      selectors.year?.value
-    );
-    setChoise("workingType");
-    deInitializeTabs();
-    setTabState("workingType", true);
-  };
-
-  // const selectedValue = useMemo(
-  //     () => all
-  // )
-
-  if (!data) return <Loader />;
-
-  return (
-    <Box sx={{ maxWidth: "500px" }}>
-      <Box sx={{ fontSize: "15px", fontWeight: "600", py: 1 }}>Институт</Box>
-      <Autocomplete
-        // placeholder="Выберите институт"
-        // isClearable
-        value={selectors.faculty}
-        onChange={handleChange("faculty")}
-        options={getOptions()}
-        data-cy="faculty-select"
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Выберите институт"
-            size="small"
-            data-cy="text-field"
-          />
-        )}
-      />
-      {selectors.faculty && (
-        <Selector
-          title="Уровень образования"
-          placeholder="Выберите уровень образования"
-          value={selectors.levelEducation}
-          onChange={handleChange("levelEducation")}
-          options={getOptionsForSelector([selectors.faculty.value])}
-          data-cy="education-level-select"
-        />
-      )}
-      {selectors.faculty && selectors.levelEducation && (
-        <Selector
-          title="Направление обучения"
-          placeholder="Выберите направление обучения"
-          value={selectors.directionOfStudy}
-          onChange={handleChange("directionOfStudy")}
-          options={getOptionsForSelector([
-            selectors.faculty.value,
-            selectors.levelEducation.value,
-          ])}
-          data-cy="direction-select"
-        />
-      )}
-      {selectors.faculty &&
-        selectors.levelEducation &&
-        selectors.directionOfStudy && (
-          <Selector
-            title="Профиль направления обучения"
-            placeholder="Выберите профиль обучения"
-            value={selectors.profile}
-            onChange={handleChange("profile")}
-            options={getOptionsForSelector([
-              selectors.faculty.value,
-              selectors.levelEducation.value,
-              selectors.directionOfStudy.value,
-            ])}
-            data-cy="profile-select"
-          />
-        )}
-      {selectors.faculty &&
-        selectors.levelEducation &&
-        selectors.directionOfStudy &&
-        selectors.profile && (
-          <Selector
-            title="Форма обучения"
-            placeholder="Выберите форму обучения"
-            value={selectors.formEducation}
-            onChange={handleChange("formEducation")}
-            options={getOptionsForSelector([
-              selectors.faculty.value,
-              selectors.levelEducation.value,
-              selectors.directionOfStudy.value,
-              selectors.profile.value,
-            ])}
-            data-cy="education-form-select"
-          />
-        )}
-      {selectors.faculty &&
-        selectors.levelEducation &&
-        selectors.directionOfStudy &&
-        selectors.profile &&
-        selectors.formEducation && (
-          <Selector
-            title="Год набора"
-            placeholder="Выберите год набора"
-            value={selectors.year}
-            onChange={handleChange("year")}
-            options={getOptionsForSelector(
-              [
-                selectors.faculty.value,
-                selectors.levelEducation.value,
-                selectors.directionOfStudy.value,
-                selectors.profile.value,
-                selectors.formEducation.value,
-              ],
-              "lastChild"
+    return (
+        <Box sx={{ maxWidth: "500px" }}>
+            <Box sx={{ fontSize: "15px", fontWeight: "600", py: 1 }}>
+                Институт
+            </Box>
+            <Autocomplete
+                // placeholder="Выберите институт"
+                // isClearable
+                value={selectors.faculty}
+                onChange={handleChange("faculty")}
+                options={getOptions()}
+                data-cy="faculty-select"
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Выберите институт"
+                        size="small"
+                        data-cy="text-field"
+                    />
+                )}
+            />
+            {selectors.faculty && (
+                <Selector
+                    title="Уровень образования"
+                    placeholder="Выберите уровень образования"
+                    value={selectors.levelEducation}
+                    onChange={handleChange("levelEducation")}
+                    options={getOptionsForSelector([selectors.faculty.value])}
+                    data-cy="education-level-select"
+                />
             )}
-            data-cy="year-select"
-          />
-        )}
-      {selectors.year && (
-        <Button variant="contained" onClick={saveTemplateData}>
-          Продолжить
-        </Button>
-      )}
-    </Box>
-  );
+            {selectors.faculty && selectors.levelEducation && (
+                <Selector
+                    title="Направление обучения"
+                    placeholder="Выберите направление обучения"
+                    value={selectors.directionOfStudy}
+                    onChange={handleChange("directionOfStudy")}
+                    options={getOptionsForSelector([
+                        selectors.faculty.value,
+                        selectors.levelEducation.value,
+                    ])}
+                    data-cy="direction-select"
+                />
+            )}
+            {selectors.faculty &&
+                selectors.levelEducation &&
+                selectors.directionOfStudy && (
+                    <Selector
+                        title="Профиль направления обучения"
+                        placeholder="Выберите профиль обучения"
+                        value={selectors.profile}
+                        onChange={handleChange("profile")}
+                        options={getOptionsForSelector([
+                            selectors.faculty.value,
+                            selectors.levelEducation.value,
+                            selectors.directionOfStudy.value,
+                        ])}
+                        data-cy="profile-select"
+                    />
+                )}
+            {selectors.faculty &&
+                selectors.levelEducation &&
+                selectors.directionOfStudy &&
+                selectors.profile && (
+                    <Selector
+                        title="Форма обучения"
+                        placeholder="Выберите форму обучения"
+                        value={selectors.formEducation}
+                        onChange={handleChange("formEducation")}
+                        options={getOptionsForSelector([
+                            selectors.faculty.value,
+                            selectors.levelEducation.value,
+                            selectors.directionOfStudy.value,
+                            selectors.profile.value,
+                        ])}
+                        data-cy="education-form-select"
+                    />
+                )}
+            {selectors.faculty &&
+                selectors.levelEducation &&
+                selectors.directionOfStudy &&
+                selectors.profile &&
+                selectors.formEducation && (
+                    <Selector
+                        title="Год набора"
+                        placeholder="Выберите год набора"
+                        value={selectors.year}
+                        onChange={handleChange("year")}
+                        options={getOptionsForSelector(
+                            [
+                                selectors.faculty.value,
+                                selectors.levelEducation.value,
+                                selectors.directionOfStudy.value,
+                                selectors.profile.value,
+                                selectors.formEducation.value,
+                            ],
+                            "lastChild"
+                        )}
+                        data-cy="year-select"
+                    />
+                )}
+            {selectors.year && (
+                <Button variant="contained" onClick={saveTemplateData}>
+                    Продолжить
+                </Button>
+            )}
+        </Box>
+    );
 };
--- rpd-client-ts/src/pages/manager/ui/Manager.tsx
+++ rpd-client-ts/src/pages/manager/ui/Manager.tsx
@@ -8,36 +8,38 @@ import { CreateRpdTemplateFrom1CExchange } from "@features/create-rpd-template-f
 import { useStore } from "@shared/hooks";
 
 export const Manager: FC = () => {
-  const { managerPage, setManagerPage } = useStore();
+    const { managerPage, setManagerPage } = useStore();
 
-  return (
-    <Container maxWidth="xl">
-      <Box component="h2" sx={{ py: 1 }}>
-        Создание комплекта РПД на основе учебного плана
-      </Box>
-      <Box
-        p={2}
-        sx={{
-          backgroundColor: "#fefefe",
-          width: "100%",
-        }}
-      >
-        {managerPage === "selectData" && (
-          <Selectors setChoise={setManagerPage} />
-        )}
-        {managerPage === "workingType" && (
-          <TemplateConstructor setChoise={setManagerPage} />
-        )}
-        {managerPage === "changeTemplate" && (
-          <ChangeRpdTemplate setChoise={setManagerPage} />
-        )}
-        {managerPage === "createTemplateFromCurrentYear" && (
-          <CreateRpdTemplateFromYear setChoise={setManagerPage} />
-        )}
-        {managerPage === "createTemplateFromExchange" && (
-          <CreateRpdTemplateFrom1CExchange setChoise={setManagerPage} />
-        )}
-      </Box>
-    </Container>
-  );
+    return (
+        <Container maxWidth="xl">
+            <Box component="h2" sx={{ py: 1 }}>
+                Создание комплекта РПД на основе учебного плана
+            </Box>
+            <Box
+                p={2}
+                sx={{
+                    backgroundColor: "#fefefe",
+                    width: "100%",
+                }}
+            >
+                {managerPage === "selectData" && (
+                    <Selectors setChoise={setManagerPage} />
+                )}
+                {managerPage === "workingType" && (
+                    <TemplateConstructor setChoise={setManagerPage} />
+                )}
+                {managerPage === "changeTemplate" && (
+                    <ChangeRpdTemplate setChoise={setManagerPage} />
+                )}
+                {managerPage === "createTemplateFromCurrentYear" && (
+                    <CreateRpdTemplateFromYear setChoise={setManagerPage} />
+                )}
+                {managerPage === "createTemplateFromExchange" && (
+                    <CreateRpdTemplateFrom1CExchange
+                        setChoise={setManagerPage}
+                    />
+                )}
+            </Box>
+        </Container>
+    );
 };
