0bfb3acadc7f6b7b538d9cce405d5381d754ae3d
Ivan Kulik <kulik6515@gmail.com>
2025-09-01 01:05:06 +0300

    Улучшение лоадера

--- rpd-client-ts/src/shared/ui/Loader.tsx
+++ rpd-client-ts/src/shared/ui/Loader.tsx
@@ -1,12 +1,20 @@
-import { Box, CircularProgress } from "@mui/material";
+import { Box } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export function Loader() {
   return (
-    <Box sx={{ p: 1, gap: 1, display: "flex" }}>
-      <CircularProgress color="inherit" size="1.5rem" />
-      <Box fontFamily={"Arial"} sx={{ px: 1, pb: 1 }}>
+    <Box
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "55%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <CircularProgress />
+      {/* <Box fontFamily={"Arial"} sx={{ px: 1, pb: 1 }}>
         Загрузка...
-      </Box>
+      </Box> */}
     </Box>
   );
 }
