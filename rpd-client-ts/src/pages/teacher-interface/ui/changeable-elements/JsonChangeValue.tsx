574c05b9f2581f29a51ec54cb190fa0c6ca78d6b
Ivan Kulik <kulik6515@gmail.com>
2025-09-01 01:17:50 +0300

    Убрал для РОПа загрузку данных с других лет и направлений

--- rpd-client-ts/src/pages/teacher-interface/ui/changeable-elements/JsonChangeValue.tsx
+++ rpd-client-ts/src/pages/teacher-interface/ui/changeable-elements/JsonChangeValue.tsx
@@ -17,6 +17,8 @@ import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import DownloadIcon from "@mui/icons-material/Download";
 import { axiosBase } from "@shared/api";
 import { DataDialogBox } from "../DataDialogBox.tsx";
+import { useAuth } from "@entities/auth";
+import { UserRole } from "@shared/ability";
 
 interface JsonChangeValue {
   elementName: string;
@@ -30,6 +32,9 @@ const JsonChangeValue: FC<JsonChangeValue> = ({ elementName }) => {
   const elementValue = useStore.getState().jsonData[elementName];
   const teacherTemplates = useStore.getState().teacherTemplates;
   const JsonData = useStore.getState().jsonData;
+
+  const userRole = useAuth.getState().userRole;
+  const isTeacher = userRole === UserRole.TEACHER;
 
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [changeableValue, setChangeableValue] = useState<string>(
@@ -164,85 +169,89 @@ const JsonChangeValue: FC<JsonChangeValue> = ({ elementName }) => {
             >
               редактировать
             </Button>
-            <IconButton
-              id="basic-button"
-              size="small"
-              aria-controls={open ? "basic-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-              onClick={handleClick}
-            >
-              <MoreHorizIcon sx={{ color: "black" }} />
-            </IconButton>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                "aria-labelledby": "basic-button",
-              }}
-            >
-              <MenuItem onClick={() => setOpenFromYearDialog(true)}>
-                <ListItemIcon>
-                  <DownloadIcon />
-                </ListItemIcon>
-                <ListItemText>
-                  <Typography
-                    variant="button"
-                    display="block"
-                    color="grey"
-                    gutterBottom
-                    m="0"
-                  >
-                    Загрузить данные из шаблона
-                    <br /> другого года
-                  </Typography>
-                </ListItemText>
-              </MenuItem>
-              <MenuItem onClick={() => setOpenFromDirectionDialog(true)}>
-                <ListItemIcon>
-                  <DownloadIcon />
-                </ListItemIcon>
-                <ListItemText>
-                  <Typography
-                    variant="button"
-                    display="block"
-                    gutterBottom
-                    color="grey"
-                    m="0"
-                  >
-                    Загрузить данные из шаблона
-                    <br /> другого направления
-                  </Typography>
-                </ListItemText>
-              </MenuItem>
-            </Menu>
+            {isTeacher && (
+              <Box>
+                <IconButton
+                  id="basic-button"
+                  size="small"
+                  aria-controls={open ? "basic-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  onClick={handleClick}
+                >
+                  <MoreHorizIcon sx={{ color: "black" }} />
+                </IconButton>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
+                >
+                  <MenuItem onClick={() => setOpenFromYearDialog(true)}>
+                    <ListItemIcon>
+                      <DownloadIcon />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <Typography
+                        variant="button"
+                        display="block"
+                        color="grey"
+                        gutterBottom
+                        m="0"
+                      >
+                        Загрузить данные из шаблона
+                        <br /> другого года
+                      </Typography>
+                    </ListItemText>
+                  </MenuItem>
+                  <MenuItem onClick={() => setOpenFromDirectionDialog(true)}>
+                    <ListItemIcon>
+                      <DownloadIcon />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <Typography
+                        variant="button"
+                        display="block"
+                        gutterBottom
+                        color="grey"
+                        m="0"
+                      >
+                        Загрузить данные из шаблона
+                        <br /> другого направления
+                      </Typography>
+                    </ListItemText>
+                  </MenuItem>
+                </Menu>
+                <DataDialogBox
+                  id={"from-year-dialog"}
+                  open={openFromYearDialog}
+                  title={"Выгрузить из другого года"}
+                  onClose={handleCloseDialog}
+                  options={teacherTemplates.filter(
+                    (option) =>
+                      option.year !== JsonData.year &&
+                      option.text === JsonData.disciplins_name
+                  )}
+                  fieldName={elementName}
+                />
+                <DataDialogBox
+                  id={"from-direction-dialog"}
+                  open={openFromDirectionDialog}
+                  title={"Выгрузить из другого направления"}
+                  onClose={handleCloseDialog}
+                  options={teacherTemplates.filter(
+                    (option) =>
+                      option.id !== JsonData.id &&
+                      option.text !== JsonData.disciplins_name
+                  )}
+                  fieldName={elementName}
+                />
+              </Box>
+            )}
           </Box>
-          <DataDialogBox
-            id={"from-year-dialog"}
-            open={openFromYearDialog}
-            title={"Выгрузить из другого года"}
-            onClose={handleCloseDialog}
-            options={teacherTemplates.filter(
-              (option) =>
-                option.year !== JsonData.year &&
-                option.text === JsonData.disciplins_name
-            )}
-            fieldName={elementName}
-          />
-          <DataDialogBox
-            id={"from-direction-dialog"}
-            open={openFromDirectionDialog}
-            title={"Выгрузить из другого направления"}
-            onClose={handleCloseDialog}
-            options={teacherTemplates.filter(
-              (option) =>
-                option.id !== JsonData.id &&
-                option.text !== JsonData.disciplins_name
-            )}
-            fieldName={elementName}
-          />
         </Box>
       )}
     </Box>
